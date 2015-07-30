29d6177694a302448ff6217dbbe58ecb26bb0799
Ruslan Alimbaev <olymp96@gmail.com>
2015-07-30 12:32:26 +0600

    Moved exceptions to separate exceptions.py file

--- decoder.py
+++ decoder.py
@@ -1,13 +1,4 @@
-class InvalidDigitSequence(Exception):
-    def __init__(self, digit_sequence):
-        self.digit_sequence = digit_sequence
-
-    def __str__(self):
-        return 'Invalid digit sequence: {}'.format(self.digit_sequence)
-
-    @property
-    def message(self):
-        return str(self)
+from exceptions import InvalidDigitSequence
 
 
 class Decoder(object):
--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,14 @@
 class HeapEmptyError(Exception):
     pass
+
+
+class InvalidDigitSequence(Exception):
+    def __init__(self, digit_sequence):
+        self.digit_sequence = digit_sequence
+
+    def __str__(self):
+        return 'Invalid digit sequence: {}'.format(self.digit_sequence)
+
+    @property
+    def message(self):
+        return str(self)
